252d333f3c8a3c40aeca5a27f0ddaf0023010df4
unLa4ky <unLa4ky@users.noreply.github.com>
2020-01-31 18:08:06 +0100

    Changed script Timer, added Opponent

--- Assets/laki/Scripts/Opponent.cs
+++ Assets/laki/Scripts/Opponent.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 
 public class Opponent : MonoBehaviour
 {
+    Timer timer;
+
     public static Opponent Instance;
     public int delay;
     private void Awake()
     {
         Instance = this;
     }
+    private void Update() {
+        timer = Timer.Instance;
+    }
     private int i;
     public bool OpponentMethod() 
     {
         i++;
-        return true;
+        return timer.Check();
     }
     private void Start()
     {
         i = 0;
         LoopsHandler.LoopDelegate opponentDelegate = OpponentMethod;
         LoopsHandler.Instance.Loop(delay, opponentDelegate);
+        
     }
     public int PointsOfEnemy()
     {
--- Assets/laki/Scripts/Timer.cs
+++ Assets/laki/Scripts/Timer.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class circle : MonoBehaviour
+public class Timer : MonoBehaviour
 {
+    public static Timer Instance;
+    private void Awake() {
+        Instance=this;
+    }
     public Image loading;
     public GameObject go;
     public int minutes;
@@ -13,6 +17,33 @@ public class circle : MonoBehaviour
     int totalSeconds = 0;
     int TOTAL_SECONDS = 0;
     float fillamount;
+    public bool TimerClock()
+    {
+        if (sec > 0)
+            sec--;
+        if (sec == 0 && minutes != 0)
+        {
+            sec = 60;
+            minutes--;
+        }
+        fillLoading();
+        return Check();
+    }
+    public bool Shake()
+    {
+        if(sec<15 && minutes==0)
+        {
+            if(tmp){
+                go.transform.position+= new Vector3(1,0,0);
+                tmp=false;
+            }
+            else {
+                go.transform.position-= new Vector3(1,0,0);
+                tmp=true;
+            }
+        }
+        return Check();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -22,50 +53,23 @@ public class circle : MonoBehaviour
         if (sec > 0)
             totalSeconds += sec;
         TOTAL_SECONDS = totalSeconds;
-        StartCoroutine(second());
-        StartCoroutine(shake());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (sec == 0 && minutes == 0)
-        {
-            StopCoroutine(second());
-            StopCoroutine(shake());
-        }
-    }
-    IEnumerator second()
-    {
-        yield return new WaitForSeconds(1f);
-        if (sec > 0)
-            sec--;
-        if (sec == 0 && minutes != 0)
-        {
-            sec = 60;
-            minutes--;
-        }
-        fillLoading();
-        StartCoroutine(second());
-    }
-    IEnumerator shake()
-    {
-        yield return new WaitForSeconds(0.1f);
-        if(sec<15 && minutes==0)
-            if(tmp){
-                go.transform.position+= new Vector3(1,0,0);
-                tmp=false;
-            }
-            else {
-                go.transform.position-= new Vector3(1,0,0);
-                tmp=true;
-            }
-        StartCoroutine(shake());
+        LoopsHandler.LoopDelegate timerDelegate = TimerClock;
+        LoopsHandler.Instance.Loop(1f, TimerClock);
+        LoopsHandler.LoopDelegate shakerDelegate = Shake;
+        LoopsHandler.Instance.Loop(0.1f, shakerDelegate);
     }
     void fillLoading()
     {
         totalSeconds--;
         float fill = (float)totalSeconds / TOTAL_SECONDS;
         loading.fillAmount= fill;
+    }
+    public bool Check()
+    {
+        if (sec == 0 && minutes == 0)
+        {
+            return false;
+        }
+        else return true;        
     }
 }
