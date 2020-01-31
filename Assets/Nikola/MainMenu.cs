13567217673a02114807602fc9a44721197081b8
unLa4ky <unLa4ky@users.noreply.github.com>
2020-01-31 22:17:38 +0100

    Added sounds for Main Menu

--- Assets/Nikola/MainMenu.cs
+++ Assets/Nikola/MainMenu.cs
@@ -6,14 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public bool OnSt()
+    {
+        AudioManager.Instance.PlaySound("backgroundSound");
+        return false;
+    }
+    private void Start() {
+        LoopsHandler.LoopDelegate onStDelegate = OnSt;
+        LoopsHandler.Instance.Loop(0.001f, OnSt);
+    }
     public void PlayGame() 
     {  
+        AudioManager.Instance.PlaySound("click");
         // Loads next scene in queue
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
+        AudioManager.Instance.PlaySound("click");
         Debug.Log("QUIT");
         Application.Quit();
     }
