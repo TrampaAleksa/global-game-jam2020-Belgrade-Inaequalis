ea9a2f8381d5a2c9ec8f2e8ef405ee650518087c
Jachovich <jacovicp@yahoo.com>
2020-02-01 21:48:14 +0100

    Dodat Raycast na ruke i ispravljene slike za prefabove na spiskovima

--- Assets/Pavle-ruke/skripte/leftHandGrab.cs
+++ Assets/Pavle-ruke/skripte/leftHandGrab.cs
@@ -4,45 +4,32 @@ using UnityEngine;
 
 public class leftHandGrab : MonoBehaviour
 {
+    
     public Sprite Grabb;
     public Sprite Normal;
-    public bool grabHold = false;
-    private Collider2D coly=null;
-
+    
+   
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey("c"))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Grabb;
-            grabHold = true;
+           
         }
         else
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Normal;
-            grabHold = false;
+            
         }
 
-        if (grabHold == false)
-        {
-          
-            coly.transform.parent = null;  //baca gresku, Object reference is not an instane of an object
-            coly = null;
-        }
-      
-    }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        print("test1");
-        coly = other;
-        if (coly.tag == "item" && grabHold == true)
-        {
-            print("test2");
-            coly.transform.parent = this.gameObject.transform;
-        }
 
-    }
+   }
+
+    
+
+
 }
-        
-  
+
+
--- Assets/Pavle-ruke/skripte/rightHandGrab.cs
+++ Assets/Pavle-ruke/skripte/rightHandGrab.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class rightHandGrab : MonoBehaviour
 {
-    bool par = false;
+   
 
     public Sprite Grabb;
     public Sprite Normal;
-    bool grabHold=false;
-    private Collider2D coly=null;
+    
+  
 
     void Awake()
     {
@@ -22,52 +22,18 @@ public class rightHandGrab : MonoBehaviour
         if (Input.GetKey("space"))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Grabb;
-            grabHold = true;
+            
             
         }
         else
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Normal;
-            grabHold = false;
-        }
-
-        if (grabHold == false)
-        {
-            coly.transform.parent = null;  //baca gresku, Object reference is not an instane of an object
-            coly = null;
-        }
-        if (par==true)
-        {
-            if (grabHold==true)
-            {
-                coly.transform.parent = this.gameObject.transform;
-            }
-            else if (grabHold==false)
-            {
-
-            }
             
+        }       
         }
      
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-       
-        if (other.tag == "item")
-        {
-            coly = other;
-            par = true;
-        }
-        print("ON TRIGER ENTER");
-        
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        par = false;
-        print("ON TRIGGER EXIT");
-    }
-
+    
    
-}
+
