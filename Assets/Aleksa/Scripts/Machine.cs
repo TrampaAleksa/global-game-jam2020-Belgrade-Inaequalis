9a87ae9a96799ceccec8db23879ec646ac039476
Aleksa Trampa <alektramp@gmail.com>
2020-02-01 01:02:03 +0100

    Code Structure created. Needs testing and creating all of the scriptable objects

---
+++ Assets/Aleksa/Scripts/Machine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Machine : StepObject
+{
+   public float workTime;
+   public Image turnedOffImage;
+   public Image turnedOnImage;
+
+   private Image currentImage;
+   public bool isTurnedOn = false;
+
+   void Start()
+   {
+       currentImage = turnedOffImage;
+   }
+
+   public void FlipSwitch(){
+       currentImage = isTurnedOn ? turnedOffImage : turnedOnImage;
+       isTurnedOn = !isTurnedOn;
+   }
+
+}
---
+++ Assets/Aleksa/Scripts/StepHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepHandler : MonoBehaviour
+{
+      public static StepHandler Instance;
+   
+    void Awake()
+    {
+        Instance = this;
+    }
+    public bool PlacedStepObject(StepObject objectPlaced, Step step){
+        if(objectPlaced.gameObject.name == step.stepObjects[step.currentStepObjectIndex].name){
+            step.currentStepObjectIndex++;
+            if(step.currentStepObjectIndex == step.stepObjects.Length) FinishStep(step);
+            else print("Correct object added, add the next one!");
+            return true;
+        }
+        else {
+            print("Wrong object added, please try again: ");
+            step.currentStepObjectIndex = 0;
+            return false;
+        }
+    }
+
+    public void FinishStep(Step step){
+        step.isFinished = true;
+    }
+}
