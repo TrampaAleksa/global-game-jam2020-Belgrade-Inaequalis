4be0ba08cc0d2b66987ecbc7ef175fb366fd9cff
Aleksa Trampa <alektramp@gmail.com>
2020-02-01 18:05:57 +0100

    RecipeTemplate code structure

---
+++ Assets/Aleksa/Scripts/RecipeTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public struct Title
+{
+    public Image image;
+    public Image text;
+}
+
+
+public class RecipeTemplate : MonoBehaviour
+{
+    public Title title;
+    public Step[] steps;
+}
