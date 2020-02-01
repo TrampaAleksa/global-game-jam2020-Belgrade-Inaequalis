9012aacc1b2fff67aba4d0f67852a74174262429
Jachovich <jacovicp@yahoo.com>
2020-02-01 03:08:51 +0100

    Hands Mechanism

---
+++ Assets/Pavle-ruke/skripte/grabb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabb : MonoBehaviour
+{
+    public Sprite Grabb;
+    public Sprite Normal;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("f"))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Grabb;
+        }else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Normal;
+        }
+        
+    }
+}
