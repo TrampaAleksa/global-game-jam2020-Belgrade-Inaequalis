a503b809382a9bbb9613c2cfd6efbd25082427b7
nikoladjordjevic <36927435+Nype@users.noreply.github.com>
2020-02-02 14:19:23 +0100

    Animacije

---
+++ Assets/Nikola/recipe/Scripts/CheckFirstStep.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckFirstStep : MonoBehaviour
+{
+    public Animator[] animators;
+ 
+    private string[] parameterNames = {
+        "firstStepIsFinished",
+        "secondStepIsFinished",
+        "thirdStepIsFinished"
+    };
+    // Update is called once per frame
+    void Update()
+    {
+        SetStepIsFinished(true,0);
+        SetStepIsFinished(true,1);
+        SetStepIsFinished(true,2);
+    }
+
+    public void SetStepIsFinished(bool isFinished, int stepIndex) // Prosledi trampin isFinished flag
+    {
+        animators[stepIndex].SetBool(parameterNames[stepIndex], isFinished);
+    }
+
+}
