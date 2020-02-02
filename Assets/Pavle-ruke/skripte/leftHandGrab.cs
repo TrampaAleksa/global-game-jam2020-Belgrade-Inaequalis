6b44a960687eda98c618b05fc9149cae6b9c104d
Jachovich <jacovicp@yahoo.com>
2020-02-02 11:48:55 +0100

    hands and background +1 item (FINAL)

--- Assets/Pavle-ruke/skripte/leftHandGrab.cs
+++ Assets/Pavle-ruke/skripte/leftHandGrab.cs
@@ -7,25 +7,37 @@ public class leftHandGrab : MonoBehaviour
     
     public Sprite Grabb;
     public Sprite Normal;
-    
-   
+    private HandGrab handGrab;
+
+    void Awake()
+    {
+        //leftHand.GetComponent<leftHandGrab>().grabHold
+    }
+
+    void Start()
+    {
+        handGrab = GetComponent<HandGrab>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("c"))
+        if (Input.GetKeyDown("c"))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Grabb;
-           
+            handGrab.TryGrabbing();
+
         }
-        else
+        if (Input.GetKeyUp("c"))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Normal;
-            
+            handGrab.ReleaseGrab();
+
         }
 
 
 
-   }
+    }
 
     
 
--- Assets/Pavle-ruke/skripte/rightHandGrab.cs
+++ Assets/Pavle-ruke/skripte/rightHandGrab.cs
@@ -8,29 +8,34 @@ public class rightHandGrab : MonoBehaviour
 
     public Sprite Grabb;
     public Sprite Normal;
+    private HandGrab handGrab;
     
   
 
     void Awake()
     {
-        //leftHand.GetComponent<leftHandGrab>().grabHold
+    }
+
+    void Start()
+    {
+        handGrab = GetComponent<HandGrab>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKeyDown("space"))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Grabb;
-            
-            
+            handGrab.TryGrabbing();
+
         }
-        else
+        if (Input.GetKeyUp("space"))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Normal;
-            
-        }       
+            handGrab.ReleaseGrab();
         }
+    }
      
     }
 
